0e4fedf8dfce49aaf2f1e2ee241fb392391161d4
Yigithan <104109200+Yigithan10@users.noreply.github.com>
2024-02-20 14:22:53 +0300

    Update App.tsx

--- mySharedElementApp/App.tsx
+++ mySharedElementApp/App.tsx
@@ -4,7 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import Detail from './src/screens/Detail';
-import {SafeAreaProvider} from 'react-native-safe-area-context'; 
+import {SafeAreaProvider} from 'react-native-safe-area-context';  
 
 const Stack = createNativeStackNavigator();
 
